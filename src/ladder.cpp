3092202729ed869d67c86153a687a192c373c800
Oliver Mao <zmao5@uci.edu>
2025-03-17 04:03:36 +0000

    seperated is_adjacent

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -5,63 +5,77 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const string& str1, const string& str2, int d) {
+    if (str1 == str2) {
+        return true;
+    }
+    
     size_t len1 = str1.length(), len2 = str2.length();
     
     if (abs(static_cast<int>(len1 - len2)) > d) {
         return false;
     }
     
-    if (d == 1) {
-        if (len1 == len2) {
-            int diffs = 0;
-            for (size_t i = 0; i < len1; i++) {
-                if (str1[i] != str2[i]) {
-                    diffs++;
-                    if (diffs > 1) return false;
-                }
-            }
-            return diffs == 1;
-        }
-        
-        const string& shorter = (len1 < len2) ? str1 : str2;
-        const string& longer = (len1 < len2) ? str2 : str1;
-        
-        size_t i = 0, j = 0;
-        bool found_diff = false;
-        
-        while (i < shorter.length() && j < longer.length()) {
-            if (shorter[i] != longer[j]) {
-                if (found_diff) return false;
-                found_diff = true;
-                j++;
-            } else {
-                i++;
-                j++;
-            }
-        }
-        
-        return (j == longer.length() || j + 1 == longer.length());
+    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
+    
+    for (size_t i = 0; i <= len1; i++) {
+        dp[i][0] = i;
     }
     
-    return false;
+    for (size_t j = 0; j <= len2; j++) {
+        dp[0][j] = j;
+    }
+    
+    for (size_t i = 1; i <= len1; i++) {
+        for (size_t j = 1; j <= len2; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+            }
+        }
+    }
+    
+    return dp[len1][len2] <= d;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
-    return edit_distance_within(word1, word2, 1);
-}
-
-void load_words(set<string>& word_list, const string& file_name) {
-    ifstream fin(file_name);
-    if (!fin) {
-        cerr << "Error: Could not open " << file_name << endl;
-        exit(1);
+    if (word1 == word2) {
+        return false;
     }
-    string word;
-    while (fin >> word) {
-        for (char& c : word) c = tolower(c);
-        word_list.insert(word);
+    
+    size_t len1 = word1.length(), len2 = word2.length();
+    int len_diff = abs(static_cast<int>(len1 - len2));
+    
+    if (len_diff > 1) {
+        return false;
     }
-    fin.close();
+    
+    if (len1 == len2) {
+        int diff_count = 0;
+        for (size_t i = 0; i < len1; i++) {
+            if (word1[i] != word2[i]) {
+                diff_count++;
+                if (diff_count > 1) {
+                    return false;
+                }
+            }
+        }
+        return diff_count == 1;
+    }
+    
+    const string& shorter = (len1 < len2) ? word1 : word2;
+    const string& longer = (len1 < len2) ? word2 : word1;
+    
+    for (size_t i = 0; i < longer.length(); i++) {
+        string temp = longer;
+        temp.erase(i, 1);
+        
+        if (temp == shorter) {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, 
@@ -71,7 +85,7 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     for (char& c : end) c = tolower(c);
     
     if (begin == end) {
-        return {begin};
+        return {};
     }
     
     if (word_list.count(end) == 0) {
@@ -91,19 +105,21 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         string last_word = current_ladder.back();
         
         for (const string& word : word_list) {
-            if (visited.count(word) > 0 || !is_adjacent(last_word, word)) {
+            if (visited.count(word) > 0) {
                 continue;
             }
             
-            vector<string> new_ladder = current_ladder;
-            new_ladder.push_back(word);
-            
-            if (word == end) {
-                return new_ladder;
+            if (is_adjacent(last_word, word)) {
+                vector<string> new_ladder = current_ladder;
+                new_ladder.push_back(word);
+                
+                if (word == end) {
+                    return new_ladder;
+                }
+                
+                visited.insert(word);
+                ladder_queue.push(new_ladder);
             }
-            
-            visited.insert(word);
-            ladder_queue.push(new_ladder);
         }
     }
     
@@ -117,10 +133,7 @@ void print_word_ladder(const vector<string>& ladder) {
     }
     cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); i++) {
-        cout << ladder[i];
-        if (i < ladder.size() - 1) {
-            cout << " ";
-        }
+        cout << ladder[i] << " ";
     }
     cout << endl;
 }
